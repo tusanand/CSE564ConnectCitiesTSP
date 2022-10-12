23712e9c4ea503d3aa44a5ac02b0fb4416fed4ce
Madhavan Raja <madhavanraja99@gmail.com>
2022-10-12 04:15:52 -0700

    Changed config variable names

--- src/Config.java
+++ src/Config.java
@@ -1,26 +1,26 @@
 import java.awt.Font;
 
 public abstract class Config {
-  public static int windowStartX = 100;
-  public static int windowStartY = 100;
-  public static int windowWidth = 1000;
-  public static int windowHeight = 700;
+  public static int WINDOW_START_X = 100;
+  public static int WINDOW_START_Y = 100;
+  public static int WINDOW_WIDTH = 1000;
+  public static int WINDOW_HEIGHT = 700;
 
-  public static Font buttonFont = new Font("Tahoma", Font.BOLD, 14);
+  public static Font BUTTON_FONT = new Font("Tahoma", Font.BOLD, 14);
 
-  public static int buttonHeight = 40;
+  public static int BUTTON_HEIGHT = 40;
 
-  public static int btnRunWidth = 180;
-  public static int btnClearWidth = 100;
-  public static int btnLoadAsymWidth = 220;
-  public static int btnLoadSymWidth = 220;
+  public static int BTN_RUN_WIDTH = 180;
+  public static int BTN_CLEAR_WIDTH = 100;
+  public static int BTN_LOAD_ASYM_WIDTH = 220;
+  public static int BTN_LOAD_SYM_WIDTH = 220;
 
-  public static int padding = 10;
-  public static int offset = 20;
+  public static int PADDING = 10;
+  public static int OFFSET = 20;
 
-  public static int pointDiameter = 4;
-  public static int highlightDiameter = 4;
+  public static int POINT_DIAMETER = 4;
+  public static int HIGHLIGHT_DIAMETER = 4;
 
-  public static int messageBoxWidth = 500;
-  public static int messageBoxHeight = 200;
+  public static int MESSAGE_BOX_WIDTH = 500;
+  public static int MESSAGE_BOX_HEIGHT = 200;
 }
--- src/InitializeFrame.java
+++ src/InitializeFrame.java
@@ -21,42 +21,42 @@ public class InitializeFrame extends ButtonActions implements InitializeFrameInt
 	public void initialize() {
 		frame = new JFrame();
 		frame.setTitle("Connect the Dots");
-		frame.setBounds(Config.windowStartX, Config.windowStartY, Config.windowWidth, Config.windowHeight);
+		frame.setBounds(Config.WINDOW_START_X, Config.WINDOW_START_Y, Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		btnRun = new JButton("Compute Distance");
-		btnRun.setFont(Config.buttonFont);
-		btnRun.setBounds(Config.padding, Config.padding, Config.btnRunWidth, Config.buttonHeight);
+		btnRun.setFont(Config.BUTTON_FONT);
+		btnRun.setBounds(Config.PADDING, Config.PADDING, Config.BTN_RUN_WIDTH, Config.BUTTON_HEIGHT);
 		btnRun.addActionListener(this);
 		frame.getContentPane().add(btnRun);
 
 		btnClear = new JButton("Clear");
-		btnClear.setFont(Config.buttonFont);
-		btnClear.setBounds(2 * Config.padding + Config.btnRunWidth, Config.padding, Config.btnClearWidth, Config.buttonHeight);
+		btnClear.setFont(Config.BUTTON_FONT);
+		btnClear.setBounds(2 * Config.PADDING + Config.BTN_RUN_WIDTH, Config.PADDING, Config.BTN_CLEAR_WIDTH, Config.BUTTON_HEIGHT);
 		btnClear.addActionListener(this);
 		frame.getContentPane().add(btnClear);
 
 		btnLoadAsymmetric = new JButton("Select Asymmetric Data");
-		btnLoadAsymmetric.setFont(Config.buttonFont);
-		btnLoadAsymmetric.setBounds(6 * Config.padding + Config.btnRunWidth + Config.btnClearWidth, Config.padding, Config.btnLoadAsymWidth, Config.buttonHeight);
+		btnLoadAsymmetric.setFont(Config.BUTTON_FONT);
+		btnLoadAsymmetric.setBounds(6 * Config.PADDING + Config.BTN_RUN_WIDTH + Config.BTN_CLEAR_WIDTH, Config.PADDING, Config.BTN_LOAD_ASYM_WIDTH, Config.BUTTON_HEIGHT);
 		btnLoadAsymmetric.addActionListener(this);
 		frame.getContentPane().add(btnLoadAsymmetric);
 
 		btnLoadSymmetric = new JButton("Select Symmetric Data");
-		btnLoadSymmetric.setFont(Config.buttonFont);
-		btnLoadSymmetric.setBounds(7 * Config.padding + Config.btnRunWidth + Config.btnClearWidth + Config.btnLoadAsymWidth, Config.padding, Config.btnLoadSymWidth, Config.buttonHeight);
+		btnLoadSymmetric.setFont(Config.BUTTON_FONT);
+		btnLoadSymmetric.setBounds(7 * Config.PADDING + Config.BTN_RUN_WIDTH + Config.BTN_CLEAR_WIDTH + Config.BTN_LOAD_ASYM_WIDTH, Config.PADDING, Config.BTN_LOAD_SYM_WIDTH, Config.BUTTON_HEIGHT);
 		btnLoadSymmetric.addActionListener(this);
 		frame.getContentPane().add(btnLoadSymmetric);
 
 		grid = new JPanel();
 		grid.setBackground(Color.WHITE);
 		grid.setBounds(
-			Config.padding,
-			2 * Config.padding + Config.buttonHeight,
-			Config.windowWidth - 10 * Config.padding,
-			Config.windowHeight - 10 * Config.padding
+			Config.PADDING,
+			2 * Config.PADDING + Config.BUTTON_HEIGHT,
+			Config.WINDOW_WIDTH - 10 * Config.PADDING,
+			Config.WINDOW_HEIGHT - 10 * Config.PADDING
 		);
 		frame.getContentPane().add(grid);
 	}
