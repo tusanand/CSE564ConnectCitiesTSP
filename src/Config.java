42b89f37056555a41e673d317af1899fde10adc5
Tushar Anand <tanand7@asu.edu>
2022-10-11 22:14:50 -0700

    Changed config variable names

--- src/Config.java
+++ src/Config.java
@@ -1,29 +1,31 @@
 import java.awt.Font;
 
 public final class Config {
+	
 	private Config() {
 		
 	}
-  public static int WINDOW_START_X = 100;
-  public static int WINDOW_START_Y = 100;
-  public static int WINDOW_WIDTH = 1000;
-  public static int WINDOW_HEIGHT = 700;
+	
+	public static int WINDOW_START_X = 100;
+	public static int WINDOW_START_Y = 100;
+	public static int WINDOW_WIDTH = 1000;
+	public static int WINDOW_HEIGHT = 700;
 
-  public static Font BUTTON_FONT = new Font("Tahoma", Font.BOLD, 14);
+	public static Font BUTTON_FONT = new Font("Tahoma", Font.BOLD, 14);
 
-  public static int BUTTON_HEIGHT = 40;
+	public static int BUTTON_HEIGHT = 40;
 
-  public static int BTN_RUN_WIDTH = 180;
-  public static int BTN_CLEAR_WIDTH = 100;
-  public static int BTN_LOAD_ASYM_WIDTH = 220;
-  public static int BTN_LOAD_SYM_WIDTH = 220;
+	public static int BTN_RUN_WIDTH = 180;
+	public static int BTN_CLEAR_WIDTH = 100;
+	public static int BTN_LOAD_ASYM_WIDTH = 220;
+	public static int BTN_LOAD_SYM_WIDTH = 220;
 
-  public static int PADDING = 10;
-  public static int OFFSET = 20;
+	public static int PADDING = 10;
+	public static int OFFSET = 20;
 
-  public static int POINT_DIAMETER = 4;
-  public static int HIGHLIGHT_DIAMETER = 4;
+	public static int POINT_DIAMETER = 4;
+	public static int HIGHLIGHT_DIAMETER = 4;
 
-  public static int MESSAGE_BOX_WIDTH = 500;
-  public static int MESSAGE_BOX_HEIGHT = 200;
+	public static int MESSAGE_BOX_WIDTH = 500;
+	public static int MESSAGE_BOX_HEIGHT = 200;
 }
